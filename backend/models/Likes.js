be63ba695241d8c7521059affa7ac5fb8dc5868d
rishabh1244 <140598487+rishabh1244@users.noreply.github.com>
2025-06-09 08:24:22 +0530

    Update Likes.js

--- backend/models/Likes.js
+++ backend/models/Likes.js
@@ -2,12 +2,15 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+
 const LikeSchema = new Schema({
     postName: { type: String, required: true },
     postAuthor: { type: String, required: true },
-    LikedBy: { type: String, required: true , unique:true},
+    LikedBy: { type: String, required: true },
     date: { type: Date, default: Date.now }
 });
+LikeSchema.index({ postName: 1, postAuthor: 1, LikedBy: 1 }, { unique: true });
+
 
 // ✅ Rename to match usage
 const Likes = mongoose.model('Like', LikeSchema); // Capital L and singular
